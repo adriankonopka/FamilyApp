5c4cea8fb2d8289b85fdae395db2f21a043f1e7e
AdrianKon <mono.mmoono@gmail.com>
2018-09-24 22:54:21 +0200

    Popup as html alert, controlling css styles from ts

--- src/app/add-father/add-father.component.ts
+++ src/app/add-father/add-father.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Father} from './Father';
 import {DataService} from '../data.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-add-father',
@@ -14,6 +15,8 @@ export class AddFatherComponent implements OnInit {
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    // this.allertt.visibility = true;
+    document.getElementById('allert').style.visibility = 'visible';
   }
 
   addFather(firstName: string, secondName: string, pesel: string, birthDate: Date) {
